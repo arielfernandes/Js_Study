852db97ae7e704af58539acb35fe63f1d800a708
arielfernandes3 <0.arielfernandes.0@gmail.com>
2021-03-11 12:18:15 -0300

    refazendo cronometro

--- cronometro/app.js
+++ cronometro/app.js
@@ -1,55 +1,44 @@
-let hour = 0
-let minute = 0
-let second = 0
-let millisecond = 0
+'use strict'
 
-let cron;
+var horas = 0;
+var minutos = 0;
+var segundos = 0;
 
-document.formMain.start.onclick = () => start()
-document.formMain.pause.onclick = () => pause()
-document.formMain.reset.onclick = () => reset()
+var tempo = 1000; // 1000 ms = 1s
+var cronometro;
 
-function start() {
-    pause()
-    cron = setInterval(() => {
-        timer()
-    }, 10)
-    console.log('start')
+document.getElementById('iniciar').addEventListener('click', () => { Start(); });
+document.getElementById('pausar').addEventListener('click', () => { Pause(); });
+document.getElementById('parar').addEventListener('click', ()=> { Stop(); });
+
+function Start() {
+    cronometro = setInterval(() => { timer(); },tempo)
 }
-function pause() {
-    clearInterval(cron)
-    console.log('pause')
+function Pause() {
+    clearInterval(cronometro);
 }
 
-function reset() {
-    
-    hour, minute, second, millisecond = 0
-    document.getElementById('hour').innerText = '00'
-    document.getElementById('minute').innerText = '00'
-    document.getElementById('second').innerText = '00'
-    document.getElementById('millisecond').innerText = '000'
-
-    console.log('reset')
+function Stop() {
+    clearInterval(cronometro);
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+    document.getElementById('counter').innerText = "00:00:00";
 }
+
 function timer() {
-    if ((millisecond += 10) == 1000) {
-        millisecond = 0
-        second++
-    }
-    if (second == 60) {
-        second = 0
-        minute++
-    }
-    if (minute == 60) {
-        minute = 0
-        hour++
+    segundos++;
+
+    if(segundos == 60) {
+        segundos = 0;
+        minutos++;
+        if(minutos == 60) {
+            minutos = 0;
+            horas++;
+        }
     }
 
-    document.getElementById('hour').innerText = returnData(hour)
-    document.getElementById('minute').innerText =  returnData(minute)
-    document.getElementById('second').innerText = returnData(second)
-    document.getElementById('millisecond').innerText = returnData(millisecond)
-}
-function returnData(input) {
-    return input > 10 ? input : `0${input}`
-}
+    var format = (horas < 10 ? '0'+horas : horas) + ':' + (minutos < 10 ? '0' + minutos : minutos) + ':' + (segundos < 10 ? '0' + segundos : segundos);
+    document.getElementById('counter').innerText = format;
+    
+}   
